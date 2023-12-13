4b8b4336bd8920324b1d6641645fddd4aeefdea0
fyi <fyi741@protonmail.com>
2023-12-13 16:11:00 +0800

    fix

--- app/Common/Core/BaseModel.php
+++ app/Common/Core/BaseModel.php
@@ -12,18 +12,14 @@ declare(strict_types=1);
 
 namespace App\Common\Core;
 
-use App\Common\Core\Entity\BaseCondition;
 use App\Common\Core\Entity\BaseModelEntity;
-use Hyperf\Database\Model\Builder;
 use Hyperf\Database\Model\SoftDeletes;
-use Hyperf\DbConnection\Db;
 use Hyperf\DbConnection\Model\Model;
-use Lengbin\Common\Entity\Page;
-use Lengbin\Helper\YiiSoft\Arrays\ArrayHelper;
 
 abstract class BaseModel extends Model
 {
     use SoftDeletes;
+    use MySQLModelTrait;
 
     abstract public function newEntity(): BaseModelEntity;
 
@@ -38,141 +34,5 @@ abstract class BaseModel extends Model
             $result[] = $model->newEntity();
         }
         return parent::newCollection($result);
-    }
-
-    public function getModelByCondition(array $condition): static
-    {
-        $tableName = '';
-        if (ArrayHelper::isValidValue($condition, '_subTable_date') && method_exists($this, 'getSubTableDate')) {
-            $tableName = $this->getSubTableDate($condition['_subTable_date']);
-        }
-        if (ArrayHelper::isValidValue($condition, '_subTable_hash') && method_exists($this, 'getSubTableHash')) {
-            $tableName = $this->getSubTableHash($condition['_subTable_hash']);
-        }
-        if (ArrayHelper::isValidValue($condition, '_subTable') && method_exists($this, 'getSubTable')) {
-            $tableName = $this->getSubTable($condition['_subTable']);
-        }
-        if (ArrayHelper::isValidValue($condition, '_table')) {
-            $tableName = $condition['_table'];
-        }
-        if ($tableName) {
-            $this->setTable($tableName);
-        }
-        return $this;
-    }
-
-    public function buildQuery(array $condition, array $search = [], array $sorts = []): Builder
-    {
-        $query = $this->getModelByCondition($condition)->newQuery();
-
-        // sort
-        if (! empty($sorts)) {
-            foreach ($sorts as $key => $value) {
-                if (empty($value)) {
-                    continue;
-                }
-                $query = $query->orderBy($key, $value);
-            }
-        }
-
-        if (empty($search)) {
-            return $query;
-        }
-
-        if (ArrayHelper::isIndexed($search)) {
-            return $query->where($search);
-        }
-
-        foreach ($search as $key => $value) {
-            if (is_null($value)) {
-                continue;
-            }
-            $query = is_array($value) ? $query->whereIn($key, $value) : $query->where($key, $value);
-        }
-        return $query;
-    }
-
-    public function output(Builder $query, array $pages): array
-    {
-        $page = ! empty($pages) ? new Page($pages) : Page::all();
-        $output = [];
-        if ($page->total) {
-            $sql = sprintf('select count(*) as count from (%s) as b', $query->toSql());
-            $output['total'] = Db::selectOne($sql, $query->getBindings())->count;
-        }
-
-        if (! $page->all) {
-            $query->forPage($page->page, $page->pageSize);
-            $output['page'] = $page->page;
-            $output['pageSize'] = $page->pageSize;
-        }
-
-        $output['list'] = $query->get()->toArray();
-        return $output;
-    }
-
-    /**
-     * @param array $data [start, end]
-     */
-    public function betweenTime(Builder $query, string $field, array $data): Builder
-    {
-        $query->where(function (Builder $builder) use ($field, $data) {
-            if ($data['start'] > 0) {
-                $builder->where($field, '>=', $data['start']);
-            }
-            if ($data['end'] > 0) {
-                $builder->where($field, '<', $data['end']);
-            }
-        });
-
-        return $query;
-    }
-
-    public function getTable(bool $isTablePrefix = false): string
-    {
-        $tableName = parent::getTable();
-        if ($isTablePrefix) {
-            $tableName = $this->getConnection()->getTablePrefix() . $tableName;
-        }
-        return $tableName;
-    }
-
-    public function batchInsert(array $data): int
-    {
-        $data = $this->appendTime($data);
-        $ret = $this->newQuery()->insert($data);
-        return $ret ? 1 : 0;
-    }
-
-    public function batchUpdate(array $data, array $column = []): int
-    {
-        $data = $this->appendTime($data, [$this->getUpdatedAtColumn()]);
-        if (empty($column)) {
-            $column = array_keys(current($data));
-        }
-        return $this->newQuery()->upsert($data, [$this->getKeyName()], $column);
-    }
-
-    private function appendTime(array $data, array $columns = []): array
-    {
-        if (empty($columns)) {
-            $columns = [$this->getCreatedAtColumn(), $this->getUpdatedAtColumn()];
-        }
-
-        $result = [];
-        $time = $this->freshTimestamp();
-        $now = $this->timestamps ? $this->fromDateTime($time) : $time->timestamp;
-        foreach ($data as $item) {
-            if ($item instanceof BaseModelEntity) {
-                $item = $item->setUnderlineName()->toArray();
-            }
-            foreach ($columns as $column) {
-                if (! ArrayHelper::keyExists($item, $column)) {
-                    $item[$column] = $now;
-                }
-            }
-            $result[] = $item;
-        }
-        return $result;
     }
 }
--- app/Service/AdminService.php
+++ app/Service/AdminService.php
@@ -15,17 +15,20 @@ namespace App\Service;
 use App\Common\Core\BaseService;
 use App\Common\Exceptions\BusinessException;
 use App\Constants\Errors\AdminError;
+use App\Infrastructure\AdminInterface;
 use App\Model\Admin;
+use App\Model\AdminEntity;
 use Lengbin\Helper\YiiSoft\Arrays\ArrayHelper;
 
-class AdminService extends BaseService
+class AdminService extends BaseService implements AdminInterface
 {
     public function __construct(protected Admin $admin) {}
 
-
-    public function getList(array $condition, array $search, array $sort, array $page, array $field = ['*']): array
+    public function getList(array $condition, array $search, array $field = ['*'], array $sort = [], array $page = []): array
     {
-        $result = $this->adminDao->getList($condition, $search, $sort, $page, $field);
+        $query = $this->admin->buildQuery($search, $sort)->select($field);
+        // todo 其他自己实现
+        $result = $this->admin->output($query, $page);
         if (ArrayHelper::isValidValue($condition, '_format')) {
             $result['list'] = $this->toArray($result['list'], function ($data) use ($condition) {
                 return $this->format($data, $condition);
@@ -34,28 +37,16 @@ class AdminService extends BaseService
         return $result;
     }
 
-    /**
-     * @param array $condition 控制参数
-     * @param array $data 数据
-     */
-    public function validate(array $condition, array $data, array $search = []): array
-    {
-        return $data;
-    }
-
-    /**
-     * @param array $data 添加数据
-     * @param array $condition 控制参数
-     */
-    public function create(array $condition, array $data): array
+    public function create(array $condition, array $data): int|string
     {
         $data = $this->validate($condition, $data);
-        $result = $this->adminDao->create($condition, $data);
+        $model = clone $this->admin;
+        $model->fill($data);
+        $ret = $model->save();
+        $result = $ret ? $model->getKey() : 0;
+
         if (! $result && ArrayHelper::isValidValue($condition, '_throw')) {
             throw new BusinessException(AdminError::CREATE_ERROR());
-        }
-        if (ArrayHelper::isValidValue($condition, '_format')) {
-            return $this->format($result, $condition);
         }
         return $result;
     }
@@ -68,8 +59,7 @@ class AdminService extends BaseService
     public function modify(array $condition, array $search, array $data): int
     {
         $data = $this->validate($condition, $data, $search);
-        $result = $this->adminDao->modify($condition, $search, $data);
-
+        $result = $this->admin->buildQuery($search)->update($data);
         if (! $result && ArrayHelper::isValidValue($condition, '_throw')) {
             throw new BusinessException(AdminError::UPDATE_ERROR());
         }
@@ -82,21 +72,22 @@ class AdminService extends BaseService
      */
     public function remove(array $condition, array $search): int
     {
-        $result = $this->adminDao->remove($condition, $search);
+        $query = $this->admin->buildQuery($search);
+        $result = ArrayHelper::isValidValue($condition, '_delete') ? $query->forceDelete() : $query->delete();
         if (! $result && ArrayHelper::isValidValue($condition, '_throw')) {
             throw new BusinessException(AdminError::DELETE_ERROR());
         }
         return $result;
     }
 
-    /**
-     * @param array $condition 控制参数
-     * @param array $search 搜索参数
-     * @param array $field 字段
-     */
-    public function detail(array $condition, array $search, array $field = ['*']): array
+    public function detail(array $condition, array $search, array $field = ['*'], array $sort = []): mixed
     {
-        $result = $this->adminDao->detail($condition, $search, $field);
+        $query = $this->admin->buildQuery($search, $sort)->select($field);
+        // todo 其他自己实现
+        /**
+         * @var AdminEntity $result
+         */
+        $result = $query->first();
         if (! $result && ArrayHelper::isValidValue($condition, '_throw')) {
             throw new BusinessException(AdminError::NOT_FOUND());
         }
@@ -106,12 +97,17 @@ class AdminService extends BaseService
         return $result;
     }
 
-    /**
-     * @param array $result 数据
-     * @param array $condition 控制参数
-     */
-    public function format(array $result, array $condition): array
+    protected function format(AdminEntity $result, array $condition): AdminEntity
     {
         return $result;
+    }
+
+    /**
+     * @param array $condition 控制参数
+     * @param array $data 数据
+     */
+    protected function validate(array $condition, array $data, array $search = []): array
+    {
+        return $data;
     }
 }
