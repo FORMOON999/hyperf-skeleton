cbb94bb3ef54b8b58ae32c445d41a5374c3aef39
fyi <fyi741@protonmail.com>
2023-12-13 17:30:09 +0800

    fix

--- app/Common/Core/Entity/BaseCondition.php
+++ app/Common/Core/Entity/BaseCondition.php
@@ -23,30 +23,6 @@ class BaseCondition extends BaseObject
     #[ApiModelProperty(value: '是否不抛异常', hidden: true)]
     public int $_throw = 1;
 
-    #[ApiModelProperty(value: '是否批量添加', hidden: true)]
-    public int $_insert = 0;
-
-    #[ApiModelProperty(value: '是否批量更新', hidden: true)]
-    public int $_update = 0;
-
     #[ApiModelProperty(value: '是否强制删除', hidden: true)]
     public int $_delete = 0;
-
-    #[ApiModelProperty(value: '是否悲观锁', hidden: true)]
-    public int $_forUpdate = 0;
-
-    #[ApiModelProperty(value: '是否排除主键', hidden: true)]
-    public int $_exceptPk = 0;
-
-    #[ApiModelProperty(value: '日期分表', hidden: true)]
-    public string $_subTable_date = '';
-
-    #[ApiModelProperty(value: 'hash分表', hidden: true)]
-    public string $_subTable_hash = '';
-
-    #[ApiModelProperty(value: '自定义分表hash', hidden: true)]
-    public string $_subTable = '';
-
-    #[ApiModelProperty(value: '指定表', hidden: true)]
-    public string $_table = '';
 }
