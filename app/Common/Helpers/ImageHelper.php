4a22471f0e031de48c55f0bda73de73e942c54da
olsai <FORMOON999@gmail.com>
2024-06-27 00:45:24 +0800

    init

--- app/Common/Helpers/ImageHelper.php
+++ app/Common/Helpers/ImageHelper.php
@@ -14,6 +14,8 @@ namespace App\Common\Helpers;
 
 class ImageHelper
 {
+    public const KEY = '864134';
+
     public function makePath(string $key, string $host): string
     {
         if (empty($key)) {
@@ -23,5 +25,32 @@ class ImageHelper
             return $key;
         }
         return rtrim($host, "\t\n\r\0\x0B/") . '/' . ltrim($key, '/');
+    }
+
+    public function encrypt(string $file, bool $saveOld = false): bool
+    {
+        if (! is_file($file)) {
+            return false;
+        }
+        $count = strlen(self::KEY);
+        $content = file_get_contents($file);
+        if ($saveOld) {
+            $oldFile = $this->getOldFile($file);
+            @file_put_contents($oldFile, $content);
+        }
+        for ($i = 0; $i < 100; ++$i) {
+            $byteValue = ord($content[$i]);
+            $byteValue ^= self::KEY[$i % $count];
+            $content[$i] = chr($byteValue);
+        }
+        @file_put_contents($file, $content);
+        return true;
+    }
+
+    public function getOldFile(string $file): string
+    {
+        $info = explode('.', $file);
+        $info[0] .= '-old';
+        return implode('.', $info);
     }
 }
